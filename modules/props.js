1519dc4da2d8ee18922deb464943901a836c730a
musicode <musicode@qq.com>
2017-05-11 16:47:57 +0800

    update

--- modules/props.js
+++ modules/props.js
@@ -1,42 +1,56 @@
 
 import * as object from 'yox-common/util/object'
 
-function updateProps(oldVnode, vnode) {
+function setProps(oldVnode, vnode) {
 
-  let oldProps = oldVnode.data.props
   let newProps = vnode.data.props
-
-  if (vnode.component || !oldProps && !newProps) {
-    return
+  if (newProps) {
+    let api = this, oldProps = oldVnode.data.props || { }
+    object.each(
+      newProps,
+      function (value, name) {
+        if (value !== oldProps[ name ]) {
+          api.setProp(vnode.el, name, value)
+          if (oldVnode.children) {
+            delete oldVnode.children
+          }
+        }
+      }
+    )
   }
-
-  oldProps = oldProps || { }
-  newProps = newProps || { }
-
-  let { el } = vnode
-  let api = this
-
-  object.each(
-    newProps,
-    function (value, name) {
-      if (value !== oldProps[ name ]) {
-        api.setProp(el, name, value)
-      }
-    }
-  )
-
-  object.each(
-    oldProps,
-    function (value, name) {
-      if (!object.has(newProps, name)) {
-        api.removeProp(el, name)
-      }
-    }
-  )
 
 }
 
+function removeProps(oldVnode, vnode) {
+
+  let oldProps = oldVnode.data.props
+  if (oldProps) {
+    let api = this, newProps = vnode.data.props || { }
+    object.each(
+      oldProps,
+      function (value, name) {
+        if (!object.has(newProps, name)) {
+          api.removeProp(vnode.el, name)
+        }
+      }
+    )
+  }
+
+}
+
+//
+// 旧 [ child1, child2 ]
+// 新 innerHTML
+//
+// 这种情况，要让外部先把 child1 child2 正常移除掉，再用 innerHTML 覆盖，否则指令无法销毁
+//
+// 旧 innerHTML
+// 新 [ child1, child2 ]
+//
+// 这种情况，先用 innerHTML 覆盖，再处理 child1 child2
+//
 export default {
-  create: updateProps,
-  update: updateProps,
+  create: setProps,
+  update: removeProps,
+  postpatch: setProps,
 }
