b61062bc9fee07ed6bbccc41307f2cf9207288c3
musicode <musicode@qq.com>
2018-07-06 10:51:19 +0800

    update

--- htmldomapi.js
+++ htmldomapi.js
@@ -22,16 +22,13 @@ attr2Prop[ 'defaultmuted' ] = 'defaultMuted'
 attr2Prop[ 'defaultselected' ] = 'defaultSelected'
 
 const svgTags = array.toObject(
-  ('svg,g,defs,desc,metadata,symbol,use,'
-  + 'image,path,rect,circle,line,ellipse,polyline,polygon,'
-  + 'text,tspan,tref,textpath,'
-  + 'marker,pattern,clippath,mask,filter,cursor,view,animate,'
-  + 'font,font-face,glyph,missing-glyph').split(',')
+  'svg,g,defs,desc,metadata,symbol,use,image,path,rect,circle,line,ellipse,polyline,polygon,text,tspan,tref,textpath,marker,pattern,clippath,mask,filter,cursor,view,animate,font,font-face,glyph,missing-glyph'.split(char.CHAR_COMMA)
 )
 
+const domain = 'http://www.w3.org/'
 const namespaces = {
-  svg: 'http://www.w3.org/2000/svg',
-  xlink: 'http://www.w3.org/1999/xlink',
+  svg: domain + '2000/svg',
+  xlink: domain + '1999/xlink',
 }
 
 export function createElement(tagName) {
