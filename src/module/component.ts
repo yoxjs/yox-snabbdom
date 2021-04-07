daebb7ddbb9c2764ed3626b1d5301e9b91579598
musicode <musicode@qq.com>
2021-04-07 19:17:02 +0800

    Update component.ts

--- src/module/component.ts
+++ src/module/component.ts
@@ -10,18 +10,11 @@ import * as object from 'yox-common/src/util/object'
 
 export function update(api: DomApi, vnode: VNode, oldVnode?: VNode) {
 
-  let { component, props, slots, model } = vnode
+  const { component, props, slots } = vnode
 
   // 更新时才要 set
   // 因为初始化时，所有这些都经过构造函数完成了
   if (component && oldVnode) {
-    if (model) {
-      if (!props) {
-        props = { }
-      }
-      props[component.$model as string] = model.value
-    }
-
     if (process.env.NODE_ENV === 'development') {
       if (props) {
         for (let key in props) {
